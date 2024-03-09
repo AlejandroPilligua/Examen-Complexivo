56798932cbe9e24f1494c8bedf72da34b41dd831
AlejandroPilligua <wapilligua@espe.edu.ec>
2024-03-09 10:37:18 -0500

    Citas_Medicas

--- FullStack_Cita_Medica/Back_cm/models/BlogModel.js
+++ FullStack_Cita_Medica/Back_cm/models/BlogModel.js
@@ -3,9 +3,9 @@ import db from "../database/db.js";
 //importamos sequelize
 import { DataTypes } from "sequelize";
 
- const BlogModel = db.define('blogs', {
-     title: { type: DataTypes.STRING },
-     content: { type: DataTypes.STRING },
+ const BlogModel = db.define('Medico', {
+     Especialida: { type: DataTypes.STRING },
+     Medico: { type: DataTypes.STRING },
  })
 
  export default BlogModel
